b5f74c2ede892ae980d68d0ca05455df96dc73ee
OSpoon <zxin088@gmail.com>
2024-06-19 23:27:29 +0800

    init: Update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -4,9 +4,8 @@ import antfu from '@antfu/eslint-config'
 export default antfu(
   {
     ignores: [
-      'src/samples/*',
-      'src/templates/*',
       // eslint ignore globs here
+      'src/templates/*',
     ],
   },
   {
