e9a2528585a17d96c9c4edb6c34109703137f1c4
OSpoon <zxin088@gmail.com>
2024-06-25 23:10:34 +0800

    Update write function

--- src/steps/buildTemplate.ts
+++ src/steps/buildTemplate.ts
@@ -1,4 +1,5 @@
 import path from 'node:path'
+import fs from 'node:fs'
 import { getTemplateDir } from '..'
 import { RENAME_FILES } from '../helpers/constants'
 import { copy } from '../helpers/copy'
@@ -9,9 +10,12 @@ import { writeTemplateFiles } from './writeTemplateFiles'
 export default async function buildTemplate(root: string, template: string, packageName: string | undefined) {
   const templateDir = getTemplateDir(template)
 
-  const write = (file: string) => {
+  const write = (file: string, content: string) => {
     const targetPath = path.join(root, RENAME_FILES[file] ?? file)
-    copy(path.join(templateDir, file), targetPath)
+    if (content)
+      fs.writeFileSync(targetPath, content)
+    else
+      copy(path.join(templateDir, file), targetPath)
   }
 
   writeTemplateFiles(write, templateDir)
